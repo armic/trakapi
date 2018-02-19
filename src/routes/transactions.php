ed68b9cd479f63f42d3a7c06a90ac5fd5bf9c761
armic <artolentino@gmail.com>
2018-02-19 11:02:16 +0800

    Transaction | kit/tool add and remove reservation routes

--- src/routes/transactions.php
+++ src/routes/transactions.php
@@ -455,6 +455,95 @@ $app->post('/api/transaction/reservation/kit/add/{custid}/{userid}/{kitid}', fun
 });
 
 
+// Add Kit  reservation
+
+$app->post('/api/transaction/reservation/kit/add/{custid}/{userid}/{kitid}', function(Request $request, Response $response) {
+
+    $custid = $request->getAttribute('custid');
+    $userid = $request->getAttribute('userid');
+    $kitid = $request->getAttribute('kitidid');
+    $reservationdate  = date("Y-m-d");
+    $reservationtime = date("h:i:sa");
+
+    $flag = 0;
+
+    $reservation = null;
+
+    // Select statement
+    // $sql = "INSERT INTO users (custid,active,auditrak,role,userid,level) VALUES (:custid,:active, :auditrak, :role, :userid, :
+    $sql = "INSERT INTO reservations (reservationdate, reservationtime, custid, userid,kitid,flag) VALUES (:reservationdate, :reservationtime, :custid, :userid,:kitid,:flag)";
+
+    try{
+        // Get DB object
+        $db= new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':custid', $custid);
+        $stmt->bindParam(':userid', $userid);
+        $stmt->bindParam(':kitid', $kitid);
+        $stmt->bindParam(':reservationdate', $reservationdate);
+        $stmt->bindParam(':reservationtime', $reservationtime);
+        $stmt->bindParam(':flag', $flag);
+
+
+        $stmt->execute();
+        $db = null;
+
+        //update tool reserved tah here
+        $trk = new clstrak();
+
+        $trk->updateToolReservedStatus($custid,$toolid,1);
+
+        echo '{"notice": {"Message": "KIT Reservation Added"}';
+
+    }catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+
+    }
+});
+
+
+// Remove Kit  reservation
+
+$app->delete('/api/transaction/reservation/kit/delete/{custid}/{userid}/{kitid}', function(Request $request, Response $response) {
+
+    $custid = $request->getAttribute('custid');
+    $userid = $request->getAttribute('userid');
+    $kitid = $request->getAttribute('kitid');
+
+    $flag = 0;
+
+    $reservation = null;
+
+    // Select statement
+    // $sql = "INSERT INTO users (custid,active,auditrak,role,userid,level) VALUES (:custid,:active, :auditrak, :role, :userid, :
+    $sql = "DELETE FROM reservations WHERE custid = $custid AND userid= $userid AND kitid = $kitid AND flag = 0";
+
+    try{
+        // Get DB object
+        $db= new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+
+
+        $stmt->execute();
+        $db = null;
+
+        //update tool reserved tah here
+        $trk = new clstrak();
+
+        $trk->updateKitReservedStatus($custid,$toolid,0);
+
+        echo '{"notice": {"Message": "KIT Reservation removed"}';
+
+    }catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+
+    }
+});
+
+
 // Add Tool  reservation
 
 $app->post('/api/transaction/reservation/tool/add/{custid}/{userid}/{toolid}', function(Request $request, Response $response) {
@@ -488,7 +577,53 @@ $app->post('/api/transaction/reservation/tool/add/{custid}/{userid}/{toolid}', f
 
         $stmt->execute();
         $db = null;
+
+        //update tool reserved tah here
+        $trk = new clstrak();
+
+        $trk->updateToolReservedStatus($custid,$toolid,1);
+
         echo '{"notice": {"Message": "TOOL Reservation Added"}';
+
+    }catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+
+    }
+});
+
+
+// Remove Tool  reservation
+
+$app->delete('/api/transaction/reservation/tool/delete/{custid}/{userid}/{toolid}', function(Request $request, Response $response) {
+
+    $custid = $request->getAttribute('custid');
+    $userid = $request->getAttribute('userid');
+    $toolid = $request->getAttribute('toolid');
+
+    $flag = 0;
+
+    $reservation = null;
+
+    // Select statement
+    // $sql = "INSERT INTO users (custid,active,auditrak,role,userid,level) VALUES (:custid,:active, :auditrak, :role, :userid, :
+    $sql = "DELETE FROM reservations WHERE custid = $custid AND userid= $userid AND toolid = $toolid AND flag = 0";
+
+    try{
+        // Get DB object
+        $db= new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+
+
+        $stmt->execute();
+        $db = null;
+
+        //update tool reserved tah here
+        $trk = new clstrak();
+
+        $trk->updateToolReservedStatus($custid,$toolid,0);
+
+        echo '{"notice": {"Message": "TOOL Reservation removed"}';
 
     }catch(PDOException $e){
         echo '{"error": {"text": '.$e->getMessage().'}';
